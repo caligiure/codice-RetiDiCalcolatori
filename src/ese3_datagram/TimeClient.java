1a2fadb18b2430e3b780a66741ef54d5c317faba
caligiure <caligiure74@gmail.com>
2024-03-26 19:33:34 +0100

    First commit

---
+++ src/ese3_datagram/TimeClient.java
@@ -0,0 +1,25 @@
+package ese3_datagram;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class TimeClient {
+    public static void main(String[] args) throws IOException {
+        String hostname = "localhost";
+        DatagramSocket socket = new DatagramSocket();
+        // invia la richiesta
+        byte[] buf = new byte[256];
+        InetAddress address = InetAddress.getByName(hostname);
+        DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 3575);
+        socket.send(packet);
+        // riceve la risposta
+        packet = new DatagramPacket(buf, buf.length);
+        socket.receive(packet);
+        // visualizza la risposta
+        String received = new String(packet.getData());
+        System.out.println("Response: " + received);
+        socket.close();
+    } // main
+}
---
+++ src/ese3_datagram/TimeServer.java
@@ -0,0 +1,39 @@
+package ese3_datagram;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Date;
+
+public class TimeServer {
+    public static void main(String[] args) {
+        DatagramSocket socket = null;
+        try {
+            socket = new DatagramSocket(3575);
+            int n = 1;
+            while (n <= 10) {
+                byte[] buf = new byte[256];
+                // riceve la richiesta
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                socket.receive(packet);
+                // produce la risposta
+                String dString = new Date().toString();
+                buf = dString.getBytes();
+                // invia la risposta al client
+                InetAddress address = packet.getAddress();
+                int port = packet.getPort();
+                packet = new DatagramPacket(buf, buf.length, address, port);
+                socket.send(packet);
+                n++;
+            }
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (socket != null) {
+                socket.close();
+            }
+        }
+    } // main
+}
